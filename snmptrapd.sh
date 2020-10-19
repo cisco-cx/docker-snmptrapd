6d5e2db09530c993943bd1413ebfd980a5175261
Agustin Henze <ahenze@cisco.com>
2020-10-19 17:21:51 +0200

    Use a different path for the snmptrapd.conf
    Also, wait for the config is there. The configuration can be generated
    by another process asynchronously.

--- snmptrapd.sh
+++ snmptrapd.sh
@@ -5,4 +5,11 @@ set -euo pipefail
 test -p /app/pipe_snmptrapd || mkfifo /app/pipe_snmptrapd
 exec 3<> /app/pipe_snmptrapd
 
-/usr/bin/stdbuf -i0 -o0 -e0 /usr/sbin/snmptrapd -L o -f -n -c /app/snmptrapd.conf > /app/pipe_snmptrapd
+SNMPTRAPD_CONFIG_PATH=/app/conf/snmptrapd.conf
+
+while ! test -f ${SNMPTRAPD_CONFIG_PATH}; do
+    echo "Waiting for the config ${SNMPTRAPD_CONFIG_PATH}"
+    sleep 1
+done
+
+/usr/bin/stdbuf -i0 -o0 -e0 /usr/sbin/snmptrapd -L o -f -n -c ${SNMPTRAPD_CONFIG_PATH} > /app/pipe_snmptrapd
